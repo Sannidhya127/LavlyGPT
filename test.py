67216f1784903c99dbf849eab11fd1bde52cc52a
Sannidhya <shubhradasgupta8@gmail.com>
2024-04-24 22:25:56 +0530

    changed llm

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import openai
 
-openai.api_key = 'sk-proj-ENUJa5XWN5Xf5mwniw0AT3BlbkFJxN9q5Pe3WN1Z0zJRflAd'
+openai.api_key = ""
 
 response = openai.Completion.create(
   engine="text-davinci-002",
